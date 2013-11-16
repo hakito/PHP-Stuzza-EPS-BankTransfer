64a584554895f7e07fd3fd5157d46e87dfa82622
Gerd Katzenbeisser <gerd.katzenbeisser@gmail.com>
2013-11-16 11:25:27 +0100

    Add test fo TransferIniatorDetails with OrderingCustomerOfiIdentifier

--- tests/unit/at/externet/eps_bank_transfer/TransferInitiatorDetailsTest.php
+++ tests/unit/at/externet/eps_bank_transfer/TransferInitiatorDetailsTest.php
@@ -21,4 +21,23 @@ class TransferInitiatorDetailsTest extends BaseTest
 
         $this->assertEquals($eDom->saveXML(), $aSimpleXml->asXML());
     }
+
+    public function testGenerateTransferInitiatorDetailsWithOfiIdentifier()
+    {
+        $webshopArticle = new WebshopArticle("Toaster", 1, 15000);
+        $transferMsgDetails = new TransferMsgDetails("http://10.18.70.8:7001/vendorconfirmation", "http://10.18.70.8:7001/transactionok?danke.asp", "http://10.18.70.8:7001/transactionnok?fehler.asp");
+        $transferMsgDetails->TargetWindowNok = $transferMsgDetails->TargetWindowOk = 'Mustershop';
+
+        $data = new TransferInitiatorDetails('AKLJS231534', 'topSecret', 'GAWIATW1XXX', 'Max Mustermann', 'AT611904300234573201', '1234567890ABCDEFG', 'AT1234567890XYZ', 15000, $webshopArticle, $transferMsgDetails, '2007-03-16');
+        $data->OrderingCustomerOfiIdentifier = 'TESTBANKXXX';
+        $aSimpleXml = $data->GetSimpleXml();
+
+        $eDom = new \DOMDocument();
+        $eDom->loadXML($this->GetEpsData('TransferInitiatorDetailsWithoutSignatureAndOrderingCustomerOfiIdentifier.xml'));
+        $eDom->formatOutput = true;
+        $eDom->preserveWhiteSpace = false;
+        $eDom->normalizeDocument();
+
+        $this->assertEquals($eDom->saveXML(), $aSimpleXml->asXML());
+    }
 }
